e6f58918254f73808498e95f609fcb7e71e85fb6
Aarthi Krishnan <116914004+arthikrishh@users.noreply.github.com>
2025-02-26 10:22:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
+      title: ' ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       home: const PaymentPage(),
